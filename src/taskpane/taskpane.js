9f75b7100609541d38d93f0b610592712fab0757
Akshay Thakur <akshay.181004@gmail.com>
2021-12-06 12:29:04 +0530

    first commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
+      console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+    }
+
+    // Assign event handlers and other initialization logic.
+    document.getElementById("filter-table").onclick = filterTable;
+    document.getElementById("sort-table").onclick = sortTable;
+    document.getElementById("SETTING").onclick= write;
+    document.getElementById("create-table").onclick = createTable;
+    document.getElementById("count").onclick =  clickCounter;
+    
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+  }
+});
+
+function createTable() {
+  Excel.run(function (context) {
+
+    // TODO1: Queue table creation logic here.
+    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    var expensesTable = currentWorksheet.tables.add("A10:D10", true /*hasHeaders*/);
+    expensesTable.name = "ExpensesTable";
+
+    // TODO2: Queue commands to populate the table with data.
+
+    expensesTable.getHeaderRowRange().values =
+      [["Date", "Merchant", "Category", "Amount"]];
+
+    expensesTable.rows.add(null /*add at the end*/, [
+      ["1/1/2017", "The Phone Company", "Communications", "120"],
+      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+      ["1/11/2017", "Bellows College", "Education", "350.1"],
+      ["1/15/2017", "Trey Research", "Other", "135"],
+      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+    ]);
+    // TODO3: Queue commands to format the table.
+    expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+    expensesTable.getRange().format.autofitColumns();
+    expensesTable.getRange().format.autofitRows();
+
+    return context.sync();
+  })
+    .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+}
+function sortTable() {
+  Excel.run(function (context) {
+
+    // TODO1: Queue commands to sort the table by Merchant name.
+    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+    var sortFields = [
+      {
+        key: 1,            // Merchant column
+        ascending: false,
+      }
+    ];
+
+    expensesTable.sort.apply(sortFields);
+
+    return context.sync();
+  })
+    .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+}
+
+/*
+function createTable() {
+  Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getActiveWorksheet();
+
+    // Create the headers and format them to stand out.
+    var headers = [
+      ["Product", "Quantity", "Unit Price", "Totals"]
+    ];
+    var headerRange = sheet.getRange("B2:E2");
+    headerRange.values = headers;
+    headerRange.format.fill.color = "#4472C4";
+    headerRange.format.font.color = "white";
+
+    // Create the product data rows.
+    var productData = [
+      ["Almonds", 6, 7.5],
+      ["Coffee", 20, 34.5],
+      ["Chocolate", 10, 9.56],
+    ];
+    var dataRange = sheet.getRange("B3:D5");
+    dataRange.values = productData;
+
+    // Create the formulas to total the amounts sold.
+    var totalFormulas = [
+      ["=C3 * D3"],
+      ["=C4 * D4"],
+      ["=C5 * D5"],
+      ["=SUM(E3:E5)"]
+    ];
+    var totalRange = sheet.getRange("E3:E6");
+    totalRange.formulas = totalFormulas;
+    totalRange.format.font.bold = true;
+
+    // Display the totals as US dollar amounts.
+    totalRange.numberFormat = [["$0.00"]];
+
+    return context.sync();
+  });
+}*/
+function filterTable() {
+  Excel.run(function (context) {
+
+    // TODO1: Queue commands to filter out all expense categories except
+    //        Groceries and Education.
+    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+    var categoryFilter = expensesTable.columns.getItem('Category').filter;
+    categoryFilter.applyValuesFilter(['Education', 'Groceries']);
+
+    return context.sync();
+  })
+    .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+}
+Office.context.document.settings.set('themeColor', 'BLUE');
+
+write('Current value for mySetting: ' + Office.context.document.settings.get('themeColor'));
+// Function that writes to a div with id='message' on the page.
+function write(message){
+    document.getElementById('SETTING').innerText += message;
+}
+
+function clickCounter() {
+  if(typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("count").innerHTML = "You have clicked this button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("count").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
